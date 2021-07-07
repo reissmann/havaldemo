14bc5fb6968923f2198308d63a3004dab8348800
Sven Reissmann <sven@0x80.io>
2021-07-07 21:27:36 +0200

    updates

--- main.c
+++ main.c
@@ -1,3 +1,8 @@
+/* Playing around with haval hashing algorithm
+ *
+ * Compile: gcc -o havaldemo main.c haval.c
+ * Run: ./havaldemo
+ */
 #include <stdlib.h>
 #include <stdio.h>
 #include "sph_types.h"
@@ -5,15 +10,18 @@
 
 int main(void) {
   sph_haval_context *ctx;
-	char data[] = "test";
-	char out[128];
+
+	char data[] = "test";			// the data to hash
+	char out[128];						// the final output
 
 	ctx = (sph_haval_context *) malloc(sizeof(sph_haval_context));
 	sph_haval128_3_init(ctx);
 	sph_haval128_3(ctx, data, sizeof(data) * sizeof(char));
   sph_haval128_3_close(ctx, out);
+  free(ctx);
 
 	printf("%s\n", out);
+
 	return 0;
 }
 
